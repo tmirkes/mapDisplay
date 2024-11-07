e1338d3f0657266a660bc4c91f8d9d752c78ad16
tmirkes <tlmirkes@gmail.com>
2024-11-07 13:56:52 -0600

    Connected back end randomizer to front end data viewer with user interaction.

--- scripts/mapManager.js
+++ scripts/mapManager.js
@@ -7,7 +7,7 @@ function main() {
     const placedSquares3 = {};
 
     // Get a unique key for each origin
-    let origin = `${window.location.href.substring(window.location.href.lastIndexOf("/") + 1,window.location.href.length - 5)}_`;
+    const storagePrefix = `${window.location.href}_`;
 
     // Function to create or update a square
     function createOrUpdateSquare(x, y, style, placedSquares) {
@@ -57,13 +57,13 @@ function main() {
             const offsetY = rect.top - imageContainer.getBoundingClientRect().top;
             return { style, x: offsetX, y: offsetY };
         });
-        localStorage.setItem(origin + 'markers1', JSON.stringify(markers1));
-        localStorage.setItem(origin + 'markers2', JSON.stringify(markers2));
-        localStorage.setItem(origin + 'markers3', JSON.stringify(markers3));
+        localStorage.setItem(storagePrefix + 'markers1', JSON.stringify(markers1));
+        localStorage.setItem(storagePrefix + 'markers2', JSON.stringify(markers2));
+        localStorage.setItem(storagePrefix + 'markers3', JSON.stringify(markers3));
     }
 
     function loadMarkers(placedSquares, storageKey) {
-        const markers = JSON.parse(localStorage.getItem(origin + storageKey));
+        const markers = JSON.parse(localStorage.getItem(storagePrefix + storageKey));
         if (markers) {
             markers.forEach(marker => {
                 createOrUpdateSquare(marker.x, marker.y, marker.style, placedSquares);
@@ -103,12 +103,8 @@ function main() {
 
     imageContainer.addEventListener('click', (event) => {
         const rect = mainImage.getBoundingClientRect();
-        console.log("image dimensions: " + rect.left + ", " + rect.top)
-        console.log("event dimensions: " + event.clientX + ", " + event.clientY);
-        console.log("expected x: " + ((event.clientX - rect.left) - 25));
         const x = event.clientX - rect.left - 25;
         const y = event.clientY - rect.top - 25;
-        console.log("modified x/y: " + x + "/" + y);
 
         if (document.querySelector('#squareMenu1 .active')) {
             createOrUpdateSquare(x, y, selectedSquareStyle, placedSquares1);
@@ -125,7 +121,7 @@ function main() {
             placedSquares1[style].remove();
             delete placedSquares1[style];
         }
-        localStorage.removeItem(origin + 'markers1');
+        localStorage.removeItem(storagePrefix + 'markers1');
     });
 
     document.getElementById('clearButton2').addEventListener('click', () => {
@@ -133,7 +129,7 @@ function main() {
             placedSquares2[style].remove();
             delete placedSquares2[style];
         }
-        localStorage.removeItem(origin + 'markers2');
+        localStorage.removeItem(storagePrefix + 'markers2');
     });
 
     document.getElementById('clearButton3').addEventListener('click', () => {
@@ -141,7 +137,7 @@ function main() {
             placedSquares3[style].remove();
             delete placedSquares3[style];
         }
-        localStorage.removeItem(origin + 'markers3');
+        localStorage.removeItem(storagePrefix + 'markers3');
     });
 
     // Save buttons
